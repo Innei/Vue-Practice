b15771fa3fbd6b69d58bd757176a95c5f55ca63c
Innei <tukon479@gmail.com>
2019-07-21 20:28:07 +0800

    rest

--- blog/server/admin/index.js
+++ blog/server/admin/index.js
@@ -1,74 +1,58 @@
 module.exports = app => {
   const express = require("express");
   const router = express.Router({
-    mergeParams: true // 合并父级路由
-  });
-  const Post = require("../models/Post");
-  const Category = require("../models/Category");
-
-  // 新增文章
-  router.post("/posts", async (req, res) => {
-    const post = await Post.create(req.body);
-    res.send(post);
+    mergeParams: true // 合并父级路由参数
   });
 
-  router.get("/posts", async (req, res) => {
-    const posts = await Post.find();
-    res.send(posts);
+  // 新增
+  router.post("/", async (req, res) => {
+    const model = await req.Model.create(req.body);
+    res.send(model);
   });
 
-  // 删除文章
-  router.delete("/posts/:id", async (req, res) => {
-    await Post.findByIdAndDelete(req.params.id);
+  router.get("/", async (req, res) => {
+    if (req.Model.modelName === "Category") {
+      const isEdit = req.query.edit === "false" ? false : req.query.edit;
+      const cateList = await req.Model.find().populate("parents");
+      if (isEdit) {
+        const index = cateList.findIndex(el => el._id == isEdit);
+        cateList.splice(index, 1);
+      }
+      res.send(cateList);
+      return;
+    }
+    const model = await req.Model.find();
+    res.send(model);
+  });
+
+  // 删除
+  router.delete("/:id", async (req, res) => {
+    await req.Model.findByIdAndDelete(req.params.id);
     res.send({
       status: "success",
       message: "删除成功"
     });
   });
 
-  // 文章详情
-  router.get("/posts/:id", async (req, res) => {
-    const detail = await Post.findById(req.params.id);
+  // 详情
+  router.get("/:id", async (req, res) => {
+    const detail = await req.Model.findById(req.params.id);
     res.send(detail);
   });
 
-  // 修改保存文章
-  router.post("/posts/:id", async (req, res) => {
-    const post = await Post.findByIdAndUpdate(req.params.id, req.body);
-    res.send(post);
+  // 修改保存
+  router.post("/:id", async (req, res) => {
+    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body);
+    res.send(model);
   });
 
-  router.post("/categories", async (req, res) => {
-    const category = await Category.create(req.body);
-    res.send(category);
-  });
-  // 分类列表
-  router.get("/categories", async (req, res) => {
-    const isEdit = req.query.edit === "false" ? false : req.query.edit;
-    const cateList = await Category.find().populate("parents");
-    if (isEdit) {
-      const index = cateList.findIndex(el => el._id == isEdit)
-      cateList.splice(index, 1)
-    }
-    res.send(cateList);
-  });
-  // 删除分类
-  router.delete("/categories/:id", async (req, res) => {
-    await Category.findByIdAndDelete(req.params.id);
-    res.send({
-      status: "success",
-      message: "删除成功"
-    });
-  });
-  // 分类信息
-  router.get("/categories/:id", async (req, res) => {
-    const cate = await Category.findById(req.params.id);
-    res.send(cate);
-  });
-  // 修改分类
-  router.post("/categories/:id", async (req, res) => {
-    res.send(await Category.findByIdAndUpdate(req.params.id, req.body));
-  });
-
-  app.use("/admin/api", router);
+  app.use(
+    "/admin/api/:res",
+    async (req, res, next) => {
+      const ModelName = require("inflection").classify(req.params.res);
+      req.Model = require(`../models/${ModelName}`); // 处理后挂载到请求体上
+      next();
+    },
+    router
+  );
 };
