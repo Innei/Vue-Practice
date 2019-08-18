57fa3ef56be854e7e5ab8b44a14670a7f9add28f
Innei <tukon479@gmail.com>
2019-08-18 16:42:52 +0800

    1/3 complete.

---
+++ info-card/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+body::before {
+  content: "";
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  position: absolute;
+  background: url("./assets/background.png");
+}
+</style>
+
+<script>
+import "./assets/css/kico.css";
+export default {};
+</script>
---
+++ info-card/src/config.js
@@ -0,0 +1,28 @@
+export default {
+  avatar: '',
+  name: '空気KUN',
+  location: '杭州',
+  study: '一名普通的在校大学生',
+  info: `法学专业在读，日语自学中，喜欢的东西是摄影、动漫、日本文学和音乐。
+
+愿你在此驻足片刻，沏一壶茶，享受一分宁静。
+
+遇见你很高兴～
+
+City of stars, are you shinning just for me ?
+
+——La La Land`,
+  societies: [
+    {
+      name: 'ins',
+      url: 'https://www.instagram.com/billriku/',
+      value: '@billriku'
+    },
+    {
+      name: 'Gmail',
+      url: 'mailto:billah109lu@gmail.com',
+      value: 'billah109lu@gmail.com'
+    }
+  ],
+  other: {}
+}
